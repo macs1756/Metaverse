247a86324f7875f1d94e502f303640c35503f3fd
macs1756 <kindrativmaksum312@gmail.com>
2022-05-09 15:59:39 +0200

    actv

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 
 console.log("hallo world");
+
+const burger = document.querySelector("#burger");
+const burger_list = document.querySelector("#burger_list");
+const burger_none = document.querySelector("#burger_none");
+
+burger.addEventListener("click", function(){
+		burger_list.classList.remove("trans");
+});
+
+burger_none.addEventListener("click", function(){
+	burger_list.classList.add("trans");
+});
+
