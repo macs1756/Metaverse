20ace31adffeafcf1b6faa7d2e1a819c3432b086
macs1756 <kindrativmaksum312@gmail.com>
2022-05-13 14:15:02 +0200

    ii

--- js/anim.js
+++ js/anim.js
@@ -1,2 +1,7 @@
 const anim = document.querySelector("#animeBtn");
 const animCard = document.querySelector("#animCard")
+
+
+anim.addEventListener("click", function(){
+	animCard.classList.toggle("animation");	
+});
